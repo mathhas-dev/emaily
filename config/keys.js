94079ec08c1fd6313222af85230028ef271ba61e
msilva@bedu.tech <msilva@bedu.tech>
2021-12-28 18:04:36 -0300

    Authentication with cookies (only google)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
     googleClientID: '151067058648-jme9gpnncqiseg3ig3r0vptvv03mpdt5.apps.googleusercontent.com',
-    googleClientSecret: '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'
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://mathkeka:XVNjKgudFnvli3jZ@emaily.ms3w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'iuysuioysuaiyiusaydoasiudoiasudsaioduasoidu'
 }
