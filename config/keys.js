6bf401f8edba3aee067117d45ce561a3a949be5d
msilva@bedu.tech <msilva@bedu.tech>
2021-12-29 13:34:19 -0300

    git rm -r --cached .

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '151067058648-jme9gpnncqiseg3ig3r0vptvv03mpdt5.apps.googleusercontent.com',
-    googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://mathkeka:XVNjKgudFnvli3jZ@emaily.ms3w9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey: 'iuysuioysuaiyiusaydoasiudoiasudsaioduasoidu',
-    linkedinClientID: '77ytwun97kvr4q',
-    linkedinClientSecret: '9NXnudWTAZv6XhZ9'
-}
