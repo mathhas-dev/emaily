eb5bed815085a3aa47703916b5c59e5ca8e4ff87
msilva@bedu.tech <msilva@bedu.tech>
2021-12-30 15:44:12 -0300

    Improving syntax proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,15 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 module.exports = function (app) {
     app.use(
-        ["/api", "/auth/google"],
-        createProxyMiddleware({
-            target: "http://localhost:5000",
-        })
-    );
-    app.use(
-        ["/api", "/auth/linkedin"],
+        ["/api", "/auth/google", "/auth/linkedin"],
         createProxyMiddleware({
             target: "http://localhost:5000",
         })
     );
 };
+
+// Remember: It's not needed create a proxy for production, because in production, 
+// the app uses just one server (express API) and the public assets (build)
