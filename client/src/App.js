f0dc58e165c4c8dddbc52b6cbf83e53d023b9b23
msilva@bedu.tech <msilva@bedu.tech>
2021-12-30 15:44:26 -0300

    Add Oauth buttons

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="/auth/google"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sign in with Google
         </a>
+
+        <a
+          className="App-link"
+          href="/auth/linkedin"
+          rel="noopener noreferrer"
+        >
+          Sign in with LinkedIn
+        </a>
+
       </header>
     </div>
   );
