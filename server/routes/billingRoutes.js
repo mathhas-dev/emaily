0245c5dfe860edfb8c9a825ffacdb93f3243478b
Matheus Henrique <mathkeka@gmail.com>
2022-01-10 17:09:02 -0300

    Require login middleware implementation

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,11 +1,9 @@
 const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
-        if (!req.user) {
-            return res.status(401).send({ error: "User not authenticated!" });
-        }
+    app.post('/api/stripe', requireLogin, async (req, res) => {
 
         const charge = await stripe.charges.create({
             amount: 500,
