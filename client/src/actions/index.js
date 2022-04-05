010115e0918ab84c5fe6cdaec3acf1d721de9c0a
Matheus Henrique <mathkeka@gmail.com>
2022-04-05 16:37:40 -0300

    Listing surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
 
     dispatch({
-        type: 'fetch_user', payload: res.data
+        type: FETCH_USER, payload: res.data
     })
 };
 
@@ -12,7 +13,7 @@ export const handleToken = (token) => async dispatch => {
     const res = await axios.post('/api/stripe', token);
 
     dispatch({
-        type: 'fetch_user', payload: res.data
+        type: FETCH_USER, payload: res.data
     })
     // Note: The response of this handler, is the updated user model
 };
@@ -21,5 +22,11 @@ export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values);
 
     history.push('/surveys');
-    dispatch({ type: 'fetch_user', payload: res.data });
+    dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
 }
